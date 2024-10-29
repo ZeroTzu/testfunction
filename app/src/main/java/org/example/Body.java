36be6ce352d528287b026d41fb1b7fc552870098
ZeroTzu <116237261+ZeroTzu@users.noreply.github.com>
2024-10-30 01:39:35 +0800

    ttest

---
+++ app/src/main/java/org/example/Body.java
@@ -0,0 +1,21 @@
+public class Body {
+    private String imageBase64;
+    private int age;
+
+    // Default constructor
+    public Body() {}
+
+    // Constructor with fields (optional)
+    public Body(String imageBase64 ) {
+        this.imageBase64 = imageBase64;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return imageBase64;
+    }
+
+    public void setName(String name) {
+        this.imageBase64 = name;
+    }
+}
